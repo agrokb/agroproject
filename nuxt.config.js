6ceb300799b21f8a963072ef25ea78345e771cf8
larry <dh821020@gmail.com>
2021-01-27 20:03:08 +0900

    finish setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,11 +2,15 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'agro',
+    router: {
+      base: 'agroproject'
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
+    target: 'static',
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
